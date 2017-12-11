880547a7384aeb721762be390af03720b6194600
Clemens <clemens.klein@student-hsrm.de>
2017-12-11 18:15:48 +0100

    Aufgabe 23 fertig

--- Blatt07/a23.py
+++ Blatt07/a23.py
@@ -2,8 +2,12 @@
 #-*- coding:UTF-8-*-
 
 i = 0
-a = list(range(0,100))
+a = list(range(0,101))
 
-
+print(a[0:10])
+print(a[91:101])
+print(a[0::10])
+print(a[(int)(a.__len__() / 2)])
+print(a[:])
 
 
