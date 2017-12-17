57cb68f1fe9892ce5b610b68f5d6fc071d314dbb
Clemens <clemens.klein@student-hsrm.de>
2017-12-17 16:03:03 +0100

    a31 erstellt

--- Blatt08/a31.py
+++ Blatt08/a31.py
@@ -1,3 +1,15 @@
 #!user/bin/python3
 #-*- coding:UTF-8-*-
 
+import sys
+
+argc = len(sys.argv)
+progname = sys.argv[0]
+argumente = sys.argv[1:]
+
+file = open(argumente[0])
+print(file.read())
+
+
+
+
