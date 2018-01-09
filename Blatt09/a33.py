9017aba112a72b21b7502a10a327f0ce6ee78d5a
change_your_name <change_your_email_adress@medieninf.de>
2018-01-09 11:21:31 +0100

    a33 fertig

--- Blatt09/a33.py
+++ Blatt09/a33.py
@@ -1,2 +1,17 @@
 #!user/bin/python3
 # -*- coding:UTF-8 -*-
+
+print("A:")
+
+print([x*x for x in range(1,11) if x % 2 == 0])
+
+y = 1234
+
+print([x for x in range(1, y) if y % x == 0])
+
+print("B:")
+
+print(filter(lambda x: x % 2 == 0, list(map(lambda x: x*x, range(1,11)))))
+
+print(filter(lambda x: y % x == 0, list(map(lambda x:x, range(1,y)))))
+
