9c0fb16acb659bb4ea796b5ad6efc507f6c38e8c
change_your_name <change_your_email_adress@medieninf.de>
2018-01-23 15:34:38 +0100

    Blatt 11 fast fertig aber es ist ein Hurensohn also obacht

--- Blatt11/messkram/Messreihe.py
+++ Blatt11/messkram/Messreihe.py
@@ -1,7 +1,7 @@
 #!user/bin/python3
 # -*- coding:UTF-8 -*-
 
-import a39
+import Messwert
 from logging.config import listen
 import datetime
 
@@ -116,47 +116,5 @@ class MonotonieVerstossError(ValueError):
 
     
     
-if __name__ == '__main__':
-    test = Messreihe(open("messwerte.csv"))[:10]
-    test2 = Messreihe(open("messwerte2.csv"))
-    print(test2[0])
-    
-    try:
-        test.addNew(test[0])
-    except:
-        print("Deine Oma du Karsten")
 
-    
-#    print(len(test))
-   
-    
-#    print(min(test, key=lambda Messwert: Messwert.temperatur))
-#    print(max(test, key=lambda Messwert: Messwert.temperatur))
-    
-#    for ele in test:
-#        if ele.temperatur > 33.0:
-#            print(ele)
-           
-#    counter = 0
-#    for ele in test["2017"]:
- #       if ele.temperatur > 26:
-  #          counter = counter +1 
-   # print(counter)
-#    temp = []
-#    for ele in test:
-#        if int(ele.temperatur) == 17:
-#            temp.append(ele.zeitpunkt)       
-#    print(temp[len(temp) -1 ])
-                   
-#    letztedrei = test["2017-10"] + test["2017-11"] + test["2017-12"]   
-#    sum = 0
-#    for ele in letztedrei:
-#        sum = sum + ele.temperatur
-    
-#    print(sum / float(len(letztedrei)))
-        
-        
-    
-    
-            
 
---
+++ Blatt11/messtest.py
@@ -0,0 +1,3 @@
+from .. import messkram
+
+m = Messreihe("2018-01-10 14:00:01.2586", 21)
