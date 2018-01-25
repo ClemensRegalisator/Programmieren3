7e968b0e9c533ac001e7f66deb7cced4a70e534f
change_your_name <change_your_email_adress@medieninf.de>
2018-01-25 14:55:49 +0100

    a21 geschafft

--- Blatt06/a21/a21.c
+++ Blatt06/a21/a21.c
@@ -1,39 +1,103 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #define LAENGE 100
 #define ANZAHL 32
 
 
 struct ele {
-char codezeichen; /* Zeichen im verschluesselten Text */
 int anzahl; /* abs. Haeufigkeit dieses Zeichens */
+char codezeichen; /* Zeichen im verschluesselten Text */
+
 };
 
-void speichern(char line[LAENGE], struct ele dict[26]){
-    printf("%s", line[0]);
-    
-    return dict;
+
+struct ele dict[26];
+
+void run(char c, char encrypt[26], char decrypt[26]){
+	int i;
+	int bool = 1;
+	for(i = 0; i < 26; i++){
+	if(decrypt[i] == c){
+		printf("%c", encrypt[i]);
+		bool = 0;
+		break;
 }
+}
+	if(bool == 1){
+		printf("%c", c);
+	}
+}
+void speichern(char c){
+    int i = 0;
+    int index = 0;
+    struct ele *dictP = dict;
+  if(c >= 97 && c <= 122){  
+    while(dictP -> anzahl != 0){
+        if(dictP -> codezeichen == c){
+            dictP -> anzahl++;
+            i = 1;
+        }
+        index++;
+        dictP++;
+    }
+        if(i == 0){
+            dictP = malloc(sizeof(struct ele));
+            dictP->codezeichen = c;
+            dictP->anzahl = 1;
+            dict[index] = *dictP;
+        }
+    }
+        
+    }
+    
+    int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)b - *(int*)a );
+}
+
+    
+
 
 
 
 int main (int argc, char *argv[]){
     
-    struct ele dict[26];
+    char folge[] = "eniastruhdlcmogkwbzfvpjxyq";
+    char key[26] = {'0'};
+    char c;
+    int i;
+    FILE * text;
     
     
-    FILE *datei;
-  char line[ANZAHL][LAENGE];
-   int i ;
-  datei = fopen ("a21-raeuberbrief.enc", "r");
-  if (datei != NULL)
-    {
-    for(i = 0; i < ANZAHL;i++) {
-        
-        fgets(line[i],LAENGE,datei);  
-        speichern(dict, line[i]);
+    text = fopen(argv[1], "r+");
+    
+    while(!feof(text)){
+        c = fgetc(text);
+        speichern(c);
     }
-    fclose (datei);
+    
+    fclose(text);
+    
+ 
+  
+    
+    qsort(dict, 26, sizeof(struct ele), cmpfunc);
+  
+    for(i = 0; i < 26; i++){
+        key[i] = dict[i].codezeichen;
     }
+
+    
+    text = fopen(argv[1], "r+");
+    
+    while(!feof(text)){
+        c = fgetc(text);
+        run(c, folge, key);
+    }
+   
+    fclose(text);
+
+    
     
     return 0;
 }
